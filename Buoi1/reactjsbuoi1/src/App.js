6ee96ec649cbcc8ae93393dbc37f4c2e5b6f6124
KhaiNguyen1608 <nguyenkhai.huflit@gmail.com>
2022-09-17 16:59:49 +0700

    init project

---
+++ Buoi1/reactjsbuoi1/src/App.js
@@ -0,0 +1,15 @@
+
+import BaiTapThucHanhLayout from './BaiTapLayoutComponent/BaiTapThucHanhLayout.jsx'
+import Header from './BaiTapLayoutComponent/Header.jsx';
+import Footter from './BaiTapLayoutComponent/Footter.jsx'
+function App() {
+  return (
+    <>  
+        <Header/>
+        <BaiTapThucHanhLayout/>
+        <Footter/>
+    </>
+  );
+}
+
+export default App;
---
+++ Buoi1/reactjsbuoi1/src/BaiTapLayoutComponent/BaiTapThucHanhLayout.jsx
@@ -0,0 +1,79 @@
+
+import React, { Component } from 'react';
+
+class BaiTapThucHanhLayout extends Component {
+    render() {
+        return (
+            <div>
+              
+                {/* Header*/}
+                <header className="py-5">
+                    <div className="container px-lg-5">
+                    <div className="p-4 p-lg-5 bg-light rounded-3 text-left">
+                        <div className="m-4 m-lg-5">
+                        <h1 className="display-5 fw-bold">A warm welcome!</h1>
+                        <p className="fs-4">Bootstrap utility classes are used to create this jumbotron since the old component has been removed from the framework. Why create custom CSS when you can use utilities?</p>
+                        <a className="btn btn-primary btn-lg" href="#!">Call to action</a>
+                        </div>
+                    </div>
+                    </div>
+                </header>
+                {/* Page Content*/}
+                <section className="pt-4">
+                    <div className="container px-lg-5">
+                    {/* Page Features*/}
+                    <div className="row gx-lg-5">
+                        <div className="col-lg-6 col-xxl-3 mb-5">
+                        <div className="card bg-light border-0 h-100">
+                            <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                            <h2 className="fs-4 fw-bold">Fresh new layout</h2>
+                            <p className="mb-0">With Bootstrap 5, we've created a fresh new layout for this template!</p>
+                            <a className="btn btn-primary btn-lg" href="#!">Call to action</a>
+                            </div>
+                            
+                        </div>
+                        </div>
+                        <div className="col-lg-6 col-xxl-3 mb-5">
+                        <div className="card bg-light border-0 h-100">
+                            <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                            <h2 className="fs-4 fw-bold">Fresh new layout</h2>
+                            <p className="mb-0">With Bootstrap 5, we've created a fresh new layout for this template!</p>
+                            <a className="btn btn-primary btn-lg" href="#!">Call to action</a>
+                            </div>
+                            
+                        </div>
+                        </div>
+                        <div className="col-lg-6 col-xxl-3 mb-5">
+                        <div className="card bg-light border-0 h-100">
+                            <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                            <h2 className="fs-4 fw-bold">Fresh new layout</h2>
+                            <p className="mb-0">With Bootstrap 5, we've created a fresh new layout for this template!</p>
+                            <a className="btn btn-primary btn-lg" href="#!">Call to action</a>
+                            </div>
+                            
+                        </div>
+                        </div>
+                        <div className="col-lg-6 col-xxl-3 mb-5">
+                        <div className="card bg-light border-0 h-100">
+                            <div className="card-body text-center p-4 p-lg-5 pt-0 pt-lg-0">
+                            <h2 className="fs-4 fw-bold">Fresh new layout</h2>
+                            <p className="mb-0">With Bootstrap 5, we've created a fresh new layout for this template!</p>
+                            <a className="btn btn-primary btn-lg" href="#!">Call to action</a>
+                            </div>
+                            
+                        </div>
+                        </div>
+                       
+                    </div>
+                    </div>
+                </section>
+              
+              
+            </div>
+
+        );
+    }
+}
+
+export default BaiTapThucHanhLayout;
+
