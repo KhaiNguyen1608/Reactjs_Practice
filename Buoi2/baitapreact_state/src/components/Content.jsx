7e7aa9c129baf85dcb905791bbb48b9f73271cbd
KhaiNguyen1608 <nguyenkhai.huflit@gmail.com>
2022-09-22 17:06:57 +0700

    Update

--- Buoi2/baitapreact_state/src/components/Content.jsx
+++ Buoi2/baitapreact_state/src/components/Content.jsx
@@ -1,16 +1,47 @@
 import React, { Component } from 'react'
-import GlassModel from './page_body/GlassModel'
-import GlassPick from './page_body/GlassPick'
+import customData from './data/dataGlasses.json'
+
+
 export default class Content extends Component {
+  state = {
+    imgGlass: ''
+  }
+  renderGlass = () => {
+    return customData.map((item) => {
+      return (
+        <div>
+          <img key={item.id} id={item.id} src={'img/' + item.url} alt="..." width={50} height={50} onClick={()=>{
+                    this.handleChangeGlass(item.id)
+                }}/>
+        </div>
+              
+      );
+    });
+  }
+
+  handleChangeGlass = (image) => {
+    this.setState({
+      imgGlass:`./img/glassesImage/v${image}.png`
+    })
+    console.log(this.state.imgGlass)
+  }
+
   render() {
     return (
       <div>
         <div className='glass_Model'>
-          <GlassModel/>
+          <div className='d-flex flex-row justify-content-evenly align-items-center'>
+              <div className='content-left'>
+                
+              </div>
+              <div className='content-right' >
+                  <img src={this.state.imgGlass} alt="" />
+              </div>
+          </div>
         </div>
         
         <div className='glass_Pick d-flex flex-row flex-wrap justify-content-center'>
-          <GlassPick/>
+            {this.renderGlass()}
         </div>
       </div>
     )
---
+++ Buoi2/baitapreact_state/src/components/page_body/GlassEvent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class GlassEvent extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default GlassEvent;
